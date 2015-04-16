73ddee2e194130dc2c26af6559993503f62977ae
aliowen <aliowen87@gmail.com>
2015-04-16 12:55:39 +0100

    Double layer NN implementation with options for dropout

--- double_layer.py
+++ double_layer.py
@@ -4,10 +4,10 @@ from scipy import optimize, stats
 
 class NN():
 
-    def __init__(self,  hidden_layer, reg_lambda=0, epsilon=0.12,
+    def __init__(self,  hidden_layer, reg_lambda=0.0,
                  opti_method='sigmoid', maxiter=500, dropout=False, p=0.5):
         self.reg_lambda = reg_lambda
-        self.epsilon = epsilon
+        # self.epsilon = epsilon
         self.hidden_layer = hidden_layer
         self.activation_func = self.sigmoid
         self.activation_func_prime = self.sigmoid_prime
@@ -38,21 +38,23 @@ class NN():
         return np.sum(a ** 2)
 
     def rand_init(self, layer_in, layer_out):
-        theta = np.random.rand(layer_out, layer_in + 1) * 2 * self.epsilon - \
-            self.epsilon
+        epsilon = np.sqrt(6) / np.sqrt(layer_in + layer_out)
+        theta = np.random.rand(layer_out, layer_in + 1) * 2 * epsilon - epsilon
         return theta
 
-    def pack_thetas(self, t1, t2):
-        return np.concatenate((t1.reshape(-1), t2.reshape(-1)))
+    def pack_thetas(self, t1, t2, t3):
+        return np.concatenate((t1.reshape(-1), t2.reshape(-1), t3.reshape(-1)))
 
     def unpack_thetas(self, thetas, input_layer, hidden_layer, output_layer):
         t1_start = 0
         t1_end = hidden_layer * (input_layer + 1)
+        t2_end = hidden_layer * (hidden_layer + 1)
         t1 = thetas[t1_start:t1_end].reshape((hidden_layer, input_layer + 1))
-        t2 = thetas[t1_end:].reshape((output_layer, hidden_layer + 1))
-        return t1, t2
+        t2 = thetas[t1_end:t2_end].reshape((hidden_layer, hidden_layer + 1))
+        t3 = thetas[t2_end:].reshape((output_layer, hidden_layer + 1))
+        return t1, t2, t3
 
-    def feed_forward(self, X, t1, t2):
+    def feed_forward(self, X, t1, t2, t3):
         m = X.shape[0]
         if len(X.shape) == 1:
             bias = np.array(1).reshape(1, )
@@ -69,25 +71,32 @@ class NN():
         # hidden layer
         z2 = np.dot(t1, a1.T)
         a2 = self.activation_func(z2)
+
         # dropout hidden layer
         if self.dropout is True:
             r = stats.bernoulli.rvs(self.p, size=self.hidden_layer)
             a2 = a2.T * r
             a2 = a2.T
+
         a2 = np.hstack((bias, a2.T))
 
-        # output layer
+        # hidden layer 2
         z3 = np.dot(t2, a2.T)
         a3 = self.activation_func(z3)
-        return a1, z2, a2, z3, a3
+        a3 = np.hstack((bias, a3.T))
+
+        # output layer
+        z4 = np.dot(t2, a2.T)
+        a4 = self.activation_func(z3)
+        return a1, z2, a2, z3, a3, z4, a4
 
     def cost_function(self, thetas, input_layer, hidden_layer, output_layer,
                       X, y, reg_lambda):
-        t1, t2 = self.unpack_thetas(thetas, input_layer, hidden_layer, output_layer)
+        t1, t2, t3 = self.unpack_thetas(thetas, input_layer, hidden_layer, output_layer)
         m = X.shape[0]
         Y = np.eye(output_layer)[y]
 
-        _, _, _, _, h = self.feed_forward(X, t1, t2)
+        _, _, _, _, _, _, h = self.feed_forward(X, t1, t2, t3)
         J = np.sum(-Y * np.log(h).T - (1 - Y) * np.log(1 - h).T) / m
 
         # regularisation of cost
@@ -95,34 +104,38 @@ class NN():
             t1_flat = t1[:, 1:]
             t2_flat = t2[:, 1:]
             R = (self.reg_lambda / (2 * m)) * (self.sumsqr(t1_flat)
-                                                 + self.sumsqr(t2_flat))
+                                               + self.sumsqr(t2_flat))
             J = J + R
 
         return J
 
     def gradient(self, thetas, input_layer, hidden_layer, output_layer,
                  X, y, reg_lambda):
-        t1, t2 = self.unpack_thetas(thetas, input_layer, hidden_layer,
+        t1, t2, t3 = self.unpack_thetas(thetas, input_layer, hidden_layer,
                                     output_layer)
         m = X.shape[0]
         t1_flat = t1[:, 1:]
         t2_flat = t2[:, 1:]
+        t3_flat = t3[:, 1:]
         Y = np.eye(output_layer)[y]
 
         # vectorised implementation
-        a1, z2, a2, z3, a3 = self.feed_forward(X, t1, t2)
+        a1, z2, a2, z3, a3, z4, a4 = self.feed_forward(X, t1, t2, t3)
         # backprop
-        d3 = a3 - Y.T
+        d4 = a4 - Y.T
+        d3 = np.dot(d4.T, t3_flat) * self.activation_func_prime(z3).T
         d2 = np.dot(d3.T, t2_flat) * self.activation_func_prime(z2).T
 
         Theta1_grad = (1 / m) * np.dot(d2.T, a1)
         Theta2_grad = (1 / m) * np.dot(d3, a2)
+        Theta3_grad = (1 / m) * np.dot(d4, a3)
 
         if reg_lambda != 0:
             Theta1_grad[:, 1:] = Theta1_grad[:, 1:] + (reg_lambda / m) * t1_flat
             Theta2_grad[:, 1:] = Theta2_grad[:, 1:] + (reg_lambda / m) * t2_flat
+            Theta3_grad[:, 1:] = Theta3_grad[:, 1:] + (reg_lambda / m) * t3_flat
 
-        return self.pack_thetas(Theta1_grad, Theta2_grad)
+        return self.pack_thetas(Theta1_grad, Theta2_grad, Theta3_grad)
 
     def fit(self, X, y):
         num_features = X.shape[0]
@@ -130,20 +143,21 @@ class NN():
         output_layer = len(set(y))
 
         theta1_0 = self.rand_init(input_layer, self.hidden_layer)
-        theta2_0 = self.rand_init(self.hidden_layer, output_layer)
-        thetas0 = self.pack_thetas(theta1_0, theta2_0)
+        theta2_0 = self.rand_init(self.hidden_layer, self.hidden_layer)
+        theta3_0 = self.rand_init(self.hidden_layer, output_layer)
+        thetas0 = self.pack_thetas(theta1_0, theta2_0, theta3_0)
 
         options = {'maxiter': self.maxiter, 'disp': True}
         _res = optimize.minimize(self.cost_function, thetas0, jac=self.gradient,
                                  method=self.method, args=(input_layer, self.hidden_layer, output_layer,
                                                            X, y, self.reg_lambda), options=options)
-        self.t1, self.t2 = self.unpack_thetas(_res.x, input_layer, self.hidden_layer, output_layer)
+        self.t1, self.t2, self.t3 = self.unpack_thetas(_res.x, input_layer, self.hidden_layer, output_layer)
 
     def predict(self, X):
         return self.predict_proba(X).argmax(0)
 
     def predict_proba(self, X):
-        _, _, _, _, h = self.feed_forward(X, self.t1, self.t2)
+        _, _, _, _, _, _, h = self.feed_forward(X, self.t1, self.t2, self.t3)
         return h
 
 
@@ -167,7 +181,7 @@ int_y = np.array([int(q[-1]) - 1 for i, q in enumerate(y)])
 # CV split
 X_train, X_cv, y_train, y_cv = cross_validation.train_test_split(X_scaled, int_y, test_size=0.2)
 
-nn = NN(hidden_layer=50, maxiter=100, opti_method='CG', reg_lambda=2.21, dropout=True)
+nn = NN(hidden_layer=50, maxiter=100, opti_method='CG', reg_lambda=2.21)
 
 if nn.dropout is True:
     t1 = np.zeros((nn.hidden_layer, X_train.shape[1] + 1))
@@ -178,12 +192,14 @@ if nn.dropout is True:
         nn.fit(X_train, y_train)
         t1 += nn.t1
         t2 += nn.t2
+        t3 += nn.t3
     t1 = (t1 / n) * nn.p
     t2 = (t2 / n) * nn.p
-    _,_,_,_,test_results = nn.feed_forward(X_cv, t1, t2)
+    t3 = (t3 / n) * nn.p
+    _,_,_,_,_,_,test_results = nn.feed_forward(X_cv, t1, t2, t3)
 else:
     nn.fit(X_train, y_train)
-    _,_,_,_,test_results = nn.feed_forward(X_cv, nn.t1, nn.t2)
+    _,_,_,_,_,_,test_results = nn.feed_forward(X_cv, nn.t1, nn.t2, nn.t3)
 Y = np.eye(len(set(y)))[y_cv]
 logloss = - (1 / X_cv.shape[0]) * np.sum(Y.T * np.log(test_results))
 print("Score=", logloss)
@@ -194,7 +210,7 @@ print("Lambda=", nn.reg_lambda)
 # test data
 X_test = test.iloc[:, 1:94]
 X_test = (X_test - X_test.mean()) / X_test.std()
-_,_,_,_, results = nn.feed_forward(X_test, nn.t1, nn.t2)
+_,_,_,_, results = nn.feed_forward(X_test, nn.t1, nn.t2, nn.t3)
 # add id back into results
 id = np.array(range(X_test.shape[0])) + 1
 results = results.T
